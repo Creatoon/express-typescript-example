1bfcc12d14c8606d11caa3e848125aa496c9c61a
Raj Aryan <65965202+Creatoon@users.noreply.github.com>
2023-01-11 00:31:07 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.post('/:name', (req, res) => {
+  res.send({
+    status: "ok",
+    message: `${req.params.name} is added to the list`
+  }) 
+})
+
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`);
